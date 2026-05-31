9a514354f88728b3cf3b2b3a22bdfac8e9cf543e
abii16 <danielfekede1616@gmail.com>
2026-05-31 16:45:42 +0300

    style(web): overhaul passenger and driver dashboards to Mobbin-style full-screen map layouts with dark theme CartoDB maps

--- web/js/driver.js
+++ web/js/driver.js
@@ -239,8 +239,8 @@
                     minZoom: 6
                 }).setView(startPos, 14);
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
                 }).addTo(map);
 
                 driverMarker = L.marker(startPos).addTo(map);
